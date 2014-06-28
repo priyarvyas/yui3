cdda51391fbe23f0aeebaaad8b7a994276546354
priyaPromact <priya@promactinfo.com>
2014-06-28 17:43:26 +0530

    updated

--- yuiTools.js
+++ yuiTools.js
@@ -1,7 +1,9 @@
 YUI().use('node', 'event', function (Y) {
    var img = Y.one('#img'),
        input = Y.one('#input');      
-     
+    
+    Y.Node.create('<p></p>').appendTo('body').set('text', 'hello world!');
+    
     Y.one('.btn-set').on('click', function(e) {
         var value = input.get('value'),
             width = img.get('offsetWidth');
@@ -11,4 +13,31 @@ YUI().use('node', 'event', function (Y) {
             img.set('offsetWidth', value);           
         }
     });
+    
+    var list2 = Y.one('#demo2');
+
+    var onList1Click = function(e) {
+        var item = e.currentTarget;
+
+        if (list2.getStyle('display') === 'none') {
+            list2.show();
+        }
+
+        list2.append(item.cloneNode(true));
+
+    };
+
+    var onList2Click = function(e) {
+        var item = e.currentTarget;
+
+        item.remove(); // sugar for item.get('parentNode').removeChild(item);
+
+        if (list2.all('li').size() < 1) { // hide the list if its empty
+            list2.hide();
+        }
+        
+    };
+
+    Y.one('#demo').delegate('click', onList1Click, 'li');
+    Y.one('#demo2').delegate('click', onList2Click, 'li');
 });
